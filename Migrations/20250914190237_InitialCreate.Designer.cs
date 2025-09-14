b196f9a87d179615ae82acdeda34571288fbeaba
madd0g72 <massimo.cavo@gmail.com>
2025-09-14 21:25:53 +0200

    ok

--- Migrations/20250914190237_InitialCreate.Designer.cs
+++ Migrations/20250914190237_InitialCreate.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CveWebApp.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20250914184749_Baseline")]
-    partial class Baseline
+    [Migration("20250914190237_InitialCreate")]
+    partial class InitialCreate
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
