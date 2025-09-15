d48ce4bffa6379c71621261b7ee07ab3e4acba1b
madd0g72 <massimo.cavo@gmail.com>
2025-09-15 16:35:02 +0200

    j

--- Data/ApplicationDbContextFactory.cs
+++ Data/ApplicationDbContextFactory.cs
@@ -5,9 +5,6 @@ using System.IO;
 
 namespace CveWebApp.Data
 {
-    /// <summary>
-    /// Design-time factory for ApplicationDbContext to support EF Core CLI commands.
-    /// </summary>
     public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
     {
         public ApplicationDbContext CreateDbContext(string[] args)
@@ -15,12 +12,25 @@ namespace CveWebApp.Data
             IConfigurationRoot configuration = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json", optional: false)
+                .AddJsonFile("appsettings.Production.json", optional: true) // <-- add this line!
                 .Build();
 
             var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
             var connectionString = configuration.GetConnectionString("DefaultConnection");
+            var dbProvider = configuration["DatabaseProvider"] ?? "MariaDb";
 
-            builder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+            if (dbProvider.Equals("MariaDb", StringComparison.OrdinalIgnoreCase))
+            {
+                builder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+            }
+            else if (dbProvider.Equals("SqlServer", StringComparison.OrdinalIgnoreCase))
+            {
+                builder.UseSqlServer(connectionString);
+            }
+            else
+            {
+                throw new Exception($"Unsupported DatabaseProvider: {dbProvider}");
+            }
 
             return new ApplicationDbContext(builder.Options);
         }
