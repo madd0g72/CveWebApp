be2b73301d3e48134f587a49b93c63b09c631a3f
madd0g72 <massimo.cavo@gmail.com>
2025-09-15 13:29:04 +0200

    k

---
+++ Migrations/20250915112839_CheckAgain.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CveWebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class CheckAgain : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
